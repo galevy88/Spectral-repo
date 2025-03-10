d03d1d08467be347284cbe9f6649363424d9d168
galevy88 <gallevy88@gmail.com>
2025-03-10 23:16:47 +0800

    start working on gal branch

--- examples/cluster_mnist.py
+++ examples/cluster_mnist.py
@@ -20,7 +20,7 @@ def main():
     spectralnet = SpectralNet(
         n_clusters=10,
         should_use_ae=True,
-        should_use_siamese=True,
+        should_use_siamese=False,
     )
     spectralnet.fit(X, y)
     cluster_assignments = spectralnet.predict(X)
